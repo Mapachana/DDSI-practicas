ba58848edef3a6afc80c9e425c1f556fab52789f
jruib <ruizbenito@outlook.es>
2021-01-02 16:47:48 +0100

    funcion main

---
+++ Practica 3/main.py
@@ -0,0 +1,16 @@
+import pyodbc
+
+def main():
+	# Incialización de la conexión
+	cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for Oracle};Direct=True;Host=oracle0.ugr.es;Service Name= practbd.oracle0.ugr.es;User ID=x7770080;Password=x7770080')
+	cursor = cnxn.cursor()
+	
+	# Llamada a menu_principal
+	
+	# Finalización del programa:
+	cursor.close()
+	cnxn.close()
+	exit(0)
+
+# Llamada al programa principal
+main()
