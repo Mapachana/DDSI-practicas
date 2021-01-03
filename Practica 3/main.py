9509d3c2fa552403bc8c2fc5324d4229f44826fc
AntonioMerino <antoniomerino99@gmail.com>
2021-01-03 18:20:14 +0100

    Cambio usuario de la conexion

--- Practica 3/main.py
+++ Practica 3/main.py
@@ -3,12 +3,12 @@ from menu_principal import *
 
 def main():
 	# Incialización de la conexión
-	cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for Oracle};Direct=True;Host=oracle0.ugr.es;Service Name= practbd.oracle0.ugr.es;User ID=x7770080;Password=x7770080')
+	cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for Oracle};Direct=True;Host=oracle0.ugr.es;Service Name= practbd.oracle0.ugr.es;User ID=x4668124;Password=x4668124')
 	cursor = cnxn.cursor()
-	
+
 	# Llamada a menu_principal
 	menu_principal(cursor)
-	
+
 	# Finalización del programa:
 	cursor.close()
 	cnxn.close()
