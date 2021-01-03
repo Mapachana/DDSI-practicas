a8395da8a31b413920871e1a55a6b29baaafd56a
Mapachana <anabuenrua99@gmail.com>
2021-01-03 20:01:02 +0100

    ahora si añado tuplas empleados

--- Practica 3/sql/insert.sql
+++ Practica 3/sql/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO Empleado (DNI, Nombre, Apellidos, Telefono, Puesto, FechaNacimiento, NSeguridadSocial, Cuenta) VALUES ('11111111A’, 'Pepe’, 'Espagnol’, '611111111’, 'Gerente’, TO_DATE('1970-10-10’, 'YYYY-MM-DD’), '11111111’, 'ES6621000418401234567891’);
+INSERT INTO Empleado (DNI, Nombre, Apellidos, Telefono, Puesto, FechaNacimiento, NSeguridadSocial, Cuenta) VALUES ('11111111A', 'Pepe', 'Espagnol', '611111111', 'Gerente', TO_DATE('1970-10-10', 'YYYY-MM-DD'), '11111111', 'ES6621000418401234567891');
 
-INSERT INTO Empleado (DNI, Nombre, Apellidos, Telefono, Puesto, FechaNacimiento, NSeguridadSocial, Cuenta) VALUES ('22222222B’, 'Carmen’, 'San Diego’, '622222222’, 'Recepcionista’, TO_DATE('1985-12-11’, 'YYYY-MM-DD’), '22222222’, 'ES66210004184012334567892’);
+INSERT INTO Empleado (DNI, Nombre, Apellidos, Telefono, Puesto, FechaNacimiento, NSeguridadSocial, Cuenta) VALUES ('22222222B', 'Carmen', 'San Diego', '622222222', 'Recepcionista', TO_DATE('1985-12-11', 'YYYY-MM-DD'), '22222222', 'ES66210004184012334567892');
 
-INSERT INTO Empleado (DNI, Nombre, Apellidos, Telefono, Puesto, FechaNacimiento, NSeguridadSocial, Cuenta) VALUES ('33333333C’, 'Manuel’, 'Carrero’, '633333333’, 'Limpieza’, TO_DATE('1965-01-10’, 'YYYY-MM-DD’), '33333333’, 'ES66215074184012334563653’);
+INSERT INTO Empleado (DNI, Nombre, Apellidos, Telefono, Puesto, FechaNacimiento, NSeguridadSocial, Cuenta) VALUES ('33333333C', 'Manuel', 'Carrero', '633333333', 'Limpieza', TO_DATE('1965-01-10', 'YYYY-MM-DD'), '33333333', 'ES66215074184012334563653');
 
-INSERT INTO EmpleadosDeBaja (DNI) VALUES ('22222222B’);
+INSERT INTO EmpleadosDeBaja (DNI) VALUES ('22222222B');
 
