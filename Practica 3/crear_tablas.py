5b1d9428373554e14495b8ec6a24dd91ab15f39e
Ubuntu 20.04 LTS <ruizbenito@outlook.es>
2021-01-03 17:48:01 +0100

    ejecucion de triggers en crear_tablas

--- Practica 3/crear_tablas.py
+++ Practica 3/crear_tablas.py
@@ -1,5 +1,7 @@
 from listar_tablas import *
 from run_sql import run_sql
+from run_pls import *
+import os # para listar directorio pls
 
 # IMPORTANTE: Esta función elimina todas las tablas de la BD y las crea de nuevo
 def crear_tablas(cursor):
@@ -9,7 +11,15 @@ def crear_tablas(cursor):
 	for t in tablas:
 		cursor.execute("DROP TABLE " + t + " CASCADE CONSTRAINTS");
 
+	# Crear tablas
 	print("Creando nuevas tablas...")
 	run_sql(cursor, 'sql/create.sql')
+	
+	# Añadir disparadores
+	print("Añadiendo disparadores...")
+	pls_dir = 'pls'
+	pls_files = os.listdir(pls_dir)
+	for pls_file in pls_files:
+		run_pls(cursor, pls_dir + '/' + pls_file)
 	
 	print("Hecho")
