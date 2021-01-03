c0b127afaf9ef75ec3ce281ecf306b732c16f094
Mapachana <anabuenrua99@gmail.com>
2021-01-03 20:09:15 +0100

    quito tilde asdhga

--- Practica 3/sql/insert.sql
+++ Practica 3/sql/insert.sql
@@ -6,7 +6,7 @@ INSERT INTO Empleado (DNI, Nombre, Apellidos, Telefono, Puesto, FechaNacimiento,
 
 INSERT INTO EmpleadoDeBaja (DNI) VALUES ('22222222B');
 
-INSERT INTO EventoTieneLugarEn (IdentificadorEvento, IdentificadorSala, Descripción, Precio, FechaHora) VALUES ('E00000001', 'S000000001', 'Boda', '200', TO_DATE('2020-10-02 14:00', 'YYYY-MM-DD HH:MI'));
+INSERT INTO EventoTieneLugarEn (IdentificadorEvento, IdentificadorSala, Descripcion, Precio, FechaHora) VALUES ('E00000001', 'S000000001', 'Boda', '200', TO_DATE('2020-10-02 14:00', 'YYYY-MM-DD HH:MI'));
 
 INSERT INTO Sala (IdentificadorSala) VALUES ('S000000001');
 
