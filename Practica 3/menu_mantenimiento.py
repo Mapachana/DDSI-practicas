d99ba1bbfd8f189ad097a0b77b85581e9c48b970
mapachana <anabuenrua@correo.ugr.es>
2021-01-17 21:00:57 +0100

    añado funcion de asingar limpieza pero no funciona

--- Practica 3/menu_mantenimiento.py
+++ Practica 3/menu_mantenimiento.py
@@ -51,7 +51,58 @@ def reparacion_resuelta(cursor):
 	
 
 def limpieza(cursor):	
-	print("implementar")
+	dni = input('Introduce el DNI del empleado: ')
+
+	while len(dni) != 9:
+		dni = input('El DNI debe tener 9 caracteres.\nIntroduce el DNI del empleado: ')
+		
+	num_hab = input('Introduzca el numero de habitacion donde se necesita una reparacion: ')
+	while len(num_hab) != 3:
+		num_hab = input('El numero de habitacion debe tener 3 digitos.\nIntroduzca el numero de habitacion donde se necesita una reparacion: ')
+
+	diaNac = input('Introduce el dia de la incidencia: ')
+
+	while int(diaNac) <= 0 or int(diaNac) > 31:
+		diaNac = input('El dia de la limpieza debe estar comprendido entre 1 y 31.\nIntroduce el dia de limpieza: ')
+
+	mesNac = input('Introduce el mes de la limpieza: ')
+
+	while int(mesNac) <= 0 or int(mesNac) > 12:
+		mesNac = input('El mes de debe estar comprendido entre 1 y 12.\nIntroduce el mes de lalimpieza: ')
+
+	anioNac = input('Introduce el anio de la limpieza: ')
+
+	while int(anioNac) <= 0:
+		anioNac = input('El anio de debe ser un numero positivo.\nIntroduce  el anio de la limpieza: ')
+		
+	hora = input('Introduce la hora de la limpieza: ')
+
+	while int(hora) < 0 or int(hora)>12:
+		hora = input('La hora debe estar entre 0 y 12.\nIntroduce  la hora de la limpieza: ')
+		
+	minuto = input('Introduce el minuto de la limpieza: ')
+
+	while int(minuto) < 0 or int(minuto) > 59:
+		minuto = input('El minuto de debe ser un numero positivo menor a 59.\nIntroduce  el minuto de la limpieza: ')
+
+	fechaNacaux = '-'.join([anioNac, mesNac, diaNac])
+	auxhora = ':'.join([hora, minuto])
+	fechaNac = ' '.join([fechaNacaux, auxhora])
+	
+	print(fechaNac)
+	
+	identificador = "L00000004" #FIXME hay que hacer un identificador unico
+
+	datos = ["'"+identificador+"'", "'"+num_hab+"'", "'"+dni+"'", "TO_DATE('"+fechaNac+"', 'YYYY-MM-DD HH:MI')"]
+
+	sentencia = 'CALL asignar_limpieza (' + ', '.join(datos) + ')'
+	print(sentencia)
+	cursor.execute(sentencia)
+	cursor.commit()
+
+	print("Se ha aniadido el aviso de reparacion con identificador: " + datos[0])
+		
+		
 
 def provisiones(cursor):
 	print("implementar")
